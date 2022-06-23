79d3c06c214c696a4aa3647e37523945315902f3
Hessam Imen <hessam.imen@gmail.com>
2022-06-23 17:56:51 +1000

    Done with assignmnet

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,30 @@
 # code here!
+class School
+    attr_accessor :name, :roster
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+
+    def add_student(student, grade)
+        if !roster[grade]
+        roster[grade] = []
+        roster[grade] << student
+        elsif roster[grade]
+        roster[grade] << student
+        end
+        roster[grade]
+    end
+
+    def grade(grade)
+        roster[grade]
+    end
+
+    def sort
+        result = {}
+        roster.each do |grade, student|
+            result[grade] = student.sort
+        end
+        result
+    end
+end
